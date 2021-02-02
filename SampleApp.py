2d3fbfd2eba93589e1876a01c4223d3167cf5bd8
ebahls@googlemail.com <ebahls@googlemail.com>
2021-02-02 11:11:33 +0100

    add readme

--- SampleApp.py
+++ SampleApp.py
@@ -1,84 +1,96 @@
 import logging
-import requests 
+import requests
 import json
 import base64
+import qrcode
+from io import BytesIO
 
-
-logging.basicConfig(format='%(asctime)s [%(levelname)s] : %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] : %(message)s',
+                    level=logging.DEBUG)
 
 # https://pro.coinmarketcap.com/account
 COIN_API_KEY = 'ca4addc3-2e2a-4c2a-9a15-c8f9c4c929da'
 
+
 def fetching_coin_market_cap():
 
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-    parameters = {
-  'symbol':'BTC,ETH,LTC,ADA,XLM,POL,DOGE',
-  'convert':'EUR'
-    }
+    parameters = {'symbol': 'BTC,ETH,LTC,ADA,XLM,DOGE', 'convert': 'EUR'}
     headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': COIN_API_KEY,
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': COIN_API_KEY,
     }
-    r = requests.get(url,headers=headers, params=parameters)
-    assert r.status_code == 200 
+    r = requests.get(url, headers=headers, params=parameters)
+    assert r.status_code == 200
     return r.json()
+
 
 def create_dg_note(text, name, dbid, token):
     form_data = {
-            'datafile':(name ,text,'application/octet-stream'),
-            'parameter':
-            ('',json.dumps({
-                'docName':name,
-                'filename':name,
-                'format':'TXT',
-                'docTypeId': 0,
-                'docClass': {'value':'FILE', 'flag':0},
-                'isVersionable':False
-            }),'application/json')
-          }
-    query = {'isSapCompliant' : False}
-    header = {'X-ARCHIVETOKEN':token}  
-    r = requests.post(f'http://dataglobal-cs/RESTfulAPI/csrest/v1.1/dept/{dbid}/docs/', headers=header,files=form_data, params=query)
+        'datafile': (name, text, 'application/octet-stream'),
+        'parameter': ('',
+                      json.dumps({
+                          'docName': name,
+                          'filename': name,
+                          'format': 'TXT',
+                          'docTypeId': 0,
+                          'docClass': {
+                              'value': 'FILE',
+                              'flag': 0
+                          },
+                          'isVersionable': False
+                      }), 'application/json')
+    }
+    query = {'isSapCompliant': False}
+    header = {'X-ARCHIVETOKEN': token}
+    r = requests.post(
+        f'http://dataglobal-cs/RESTfulAPI/csrest/v1.1/dept/{dbid}/docs/',
+        headers=header,
+        files=form_data,
+        params=query)
     assert r.status_code == 201
     return r.json()
 
+
 def create_mark_down(data):
-    mark_down = f"# Crypto Markt {data['status']['timestamp']}\n"
-    mark_down+= create_png_md_imge('./dogecoin.png')+"\n\n"
-    mark_down+="|Symbol|Name|EUR|\n"
-    mark_down+="|------|----|---|\n"
-    
-    
-    for name,info in data['data'].items():
-        logging.info(f"fetching {info['name']} {name} {info['quote']['EUR']['price']}")
+    mark_down = f"# Crypto Markt am  {data['status']['timestamp']} \n"
+
+    mark_down += create_png_md_imge('./dogecoin.png') + "\n"
+    mark_down += "|Symbol|Name|EUR|\n"
+    mark_down += "|------|----|---|\n"
+
+   
+
+    for name, info in data['data'].items():
         mark_down += f"|{name}|{info['name']}|{info['quote']['EUR']['price']}|\n"
+
+    mark_down += create_qr_code("http:\dataglobal.de")
     return mark_down
 
+
 def create_png_md_imge(filename):
-  assert filename.endswith('png')
-  with open (filename,'rb') as f:
-    
-    return f"![icon](data:image/png;base64,{base64.b64encode(f.read()).decode('UTF-8')})"
+    with open(filename, 'rb') as f:
+        return f"![icon](data:image/png;base64,{base64.b64encode(f.read()).decode('UTF-8')})"
+
+
+def create_qr_code(txt):
+    img = qrcode.make(txt, box_size=4)
+    buffer = BytesIO()
+    img.save(buffer, format='png')
+    return f"![qrcode](data:image/png;base64,{base64.b64encode(buffer.getvalue()).decode('UTF-8')})"
+
 
 def login(user, pwd):
-
-  headers = { 'X-USERNAME':user, 'X-PASSWORD':pwd}
-  r = requests.get('http://dataglobal-cs/RESTfulAPI/csrest/v1.1/auth/logon?applClass=Common&progId=Standard',
-  headers=headers)
-  assert r.status_code == 200
-
-  return r.headers['X-ARCHIVETOKEN']
+    headers = {'X-USERNAME': user, 'X-PASSWORD': pwd}
+    r = requests.get(
+        'http://dataglobal-cs/RESTfulAPI/csrest/v1.1/auth/logon?applClass=Common&progId=Standard',
+        headers=headers)
+    assert r.status_code == 200
+    return r.headers['X-ARCHIVETOKEN']
 
 
 if __name__ == "__main__":
-    token =  login("Administrator","Data230Global")
-    logging.info(f"login token:{token}")
- 
-    #data=fetching_coin_market_cap()
-    #md = create_mark_down(data)
-
-    #logging.info(create_dg_note(md,"crypto.txt",1571768983,token))
-
-    
-   
+    token = login("Administrator", "Data230Global")
+    md = create_mark_down(fetching_coin_market_cap())
+    logging.info(token)
+    logging.info(create_dg_note(md,'crypto.txt',1571768983,token))
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Hyparchive Sample Application 
+
+This tiny python program should just demonstrate how to use the dataglobal RestApi to archive and generate nice-looking documents.
+It will download the current crypto courses and creates a document out of them.
+
+## Installation
+
+To run it you need to install Python 3.8 or greater. 
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install -r requirements.txt 
+```
+
+you also need a running dg archive instance with RestApi installed
+
+## Usage
+
+change this two variables 
+
+```python
+COIN_API_KEY = 'xxx'
+host = "localhost"
+```
+
+COIN_API_KEY is the api key you need to request from coin marked cap 
+host is the api endpoint
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Twitter :
+@mrebahls 
+<br>
+Mail : enrico.bahls@dataglobal.com 
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
